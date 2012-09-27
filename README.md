97c3c63acd8da1eb8971c9f0d06f97e13428fafc
Walter Dal Mut <walter.dalmut@gmail.com>
2012-09-27 20:02:57 +0200

    Base pluginW

---
+++ README.md
@@ -0,0 +1,11 @@
+# WordPress Twig Integration Example
+
+Just a simple example of Twig integration on WordPress plugin.
+
+First of all install dependencies using composer.
+
+```
+curl -s http://getcomposer.org/installer | php
+php composer.phar install
+```
+
---
+++ wp-simple-tweets.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Simple Tweets
+Plugin URI: https://github.com/wdalmut/wp-simple-tweets
+Description: Just a simple example to show how to integrate Twig into a WP plugin.
+Version: 0.0.1
+Author: Walter Dal Mut
+Author URI: http://www.corley.it/
+License: MIT
+*/
+
+if (!class_exists("\Composer\Autoload\ClassLoader")) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
+
+add_action('wp_dashboard_setup', 'upcloo_add_dashboard_widgets' );
+
+function upcloo_add_dashboard_widgets() A Simple{
+    wp_add_dashboard_widget('wpcon_dashboard_widget', 'A Simple News Widget', 'wpcon_dashboard_widget_function');
+}
+
+function wpcon_dashboard_widget_function() {
+    $url = 'http://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=corleycloud&count=10';
+    $unpack = @json_decode(file_get_contents($url));
+
+    $template = get_twig()->loadTemplate('tweets.twig');
+    echo $template->render(array('elements' => $unpack));
+}
+
+function get_twig()
+{
+    static $twig;
+    if (!$twig) {
+        $loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
+        $twig = new Twig_Environment($loader, array('cache' => __DIR__ . '/cache'));
+    }
+
+    return $twig;
+}
